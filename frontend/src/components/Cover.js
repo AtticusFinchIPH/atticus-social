cfd00b959163c33694579e8610e1da15db3123e9
AtticusFinchIPH <tranvan.duc.012@gmail.com>
2020-08-21 10:50:56 +0200

    Add change cover & save buttons

--- frontend/src/components/Cover.js
+++ frontend/src/components/Cover.js
@@ -1,12 +1,13 @@
 import React from "react";
 import clsx from "clsx";
 import { makeStyles } from "@material-ui/core/styles";
-import { Paper, Avatar, Typography, Badge} from "@material-ui/core";
+import { Paper, Avatar, Typography, Badge, Tooltip, IconButton} from "@material-ui/core";
 import { deepOrange, lightBlue, deepPurple } from '@material-ui/core/colors';
 import PhotoCameraIcon from '@material-ui/icons/PhotoCamera';
 import EditIcon from '@material-ui/icons/Edit';
+import SaveIcon from '@material-ui/icons/Save';
 import { getCharacterColor } from "../util";
-import { useTheme } from "@material-ui/styles";
+import { useTheme, withStyles } from "@material-ui/styles";
 
 const AVATAR_DIMENSION = 20;
 
@@ -25,7 +26,10 @@ const useStyles = makeStyles((theme) => ({
     [theme.breakpoints.up('xs')]: {
       padding: theme.spacing(10),
       paddingBottm: theme.spacing(0)
-    }
+    },
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
   },
   avatar: {
     width: theme.spacing(AVATAR_DIMENSION),
@@ -71,14 +75,22 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+const LightTooltip = withStyles((theme) => ({
+  tooltip: {
+    backgroundColor: theme.palette.common.white,
+    color: 'rgba(0, 0, 0, 0.87)',
+    boxShadow: theme.shadows[1],
+    fontSize: 14,
+  },
+}))(Tooltip);
+
 const EditAvatar = (props) => {
   const classes = useStyles();
-  const theme = useTheme();
   const { userInfo } = props;
   return (
-    <div className={classes.editAvatar}>
+    <IconButton aria-label="ChangeAvatar" className={classes.editAvatar}>
       <PhotoCameraIcon className={classes[`${getCharacterColor(userInfo.firstName.charAt(0))}Color`]} style={{ margin: "2px 0 0 2px" }} />
-    </div>
+    </IconButton>
   )
 }
 
@@ -92,47 +104,69 @@ const Cover = (props) => {
               editable 
               ?
                 <>
-                <Badge
-                  overlap="circle"
-                  anchorOrigin={{
-                    vertical: 'bottom',
-                    horizontal: 'right',
-                  }}
-                  badgeContent={<EditAvatar userInfo={userInfo}/>}
-                >
-                  <Avatar className={clsx( classes.avatar, classes[getCharacterColor(userInfo.firstName.charAt(0))])}>
-                      <Typography component="h1" variant="h3" color="inherit">
-                        {userInfo.firstName.charAt(0).toUpperCase()}
-                      </Typography>
-                  </Avatar>
-                </Badge>
-                <div className={classes.flexRow}>
-                  <Typography className={classes.text} component="h1" variant="h3" color="inherit">
-                    {`${userInfo.firstName} ${userInfo.lastName}`}
-                  </Typography>
-                  <EditIcon />
+                <div>
+                  <Badge
+                    overlap="circle"
+                    anchorOrigin={{
+                      vertical: 'bottom',
+                      horizontal: 'right',
+                    }}
+                    badgeContent={<EditAvatar userInfo={userInfo}/>}
+                  >
+                    <Avatar className={clsx( classes.avatar, classes[getCharacterColor(userInfo.firstName.charAt(0))])}>
+                        <Typography component="h1" variant="h3" color="inherit">
+                          {userInfo.firstName.charAt(0).toUpperCase()}
+                        </Typography>
+                    </Avatar>
+                  </Badge>
+                  <div className={classes.flexRow}>
+                    <Typography className={classes.text} component="h1" variant="h3" color="inherit">
+                      {`${userInfo.firstName} ${userInfo.lastName}`}
+                    </Typography>
+                    <LightTooltip title="Change nickname">
+                      <IconButton aria-label="ChangeNickname" style={{color: "#ffffff"}} >
+                        <EditIcon />
+                      </IconButton>
+                    </LightTooltip>
+                  </div>
+                  <div className={classes.flexRow}>
+                    <Typography className={classes.text} variant="h5" color="inherit" paragraph>
+                        Description
+                    </Typography>
+                    <LightTooltip title="Change description">
+                      <IconButton aria-label="ChangeDescription" style={{color: "#ffffff"}} >
+                        <EditIcon fontSize="small" />
+                      </IconButton>
+                    </LightTooltip>
+                  </div>
                 </div>
-                <div className={classes.flexRow}>
-                  <Typography className={classes.text} variant="h5" color="inherit" paragraph>
-                      Description
-                  </Typography>
-                  <EditIcon fontSize="small" />
+                <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'space-between'}}>                 
+                  <LightTooltip title="Change photo cover">
+                    <IconButton aria-label="ChangeCover" style={{color: "#ffffff"}} >
+                      <PhotoCameraIcon fontSize="large" />
+                    </IconButton>
+                  </LightTooltip>
+                  <LightTooltip title="Save changes">
+                    <IconButton aria-label="SaveChanges" style={{color: "#ffffff"}} >
+                      <SaveIcon fontSize="large" />
+                    </IconButton>
+                  </LightTooltip>
                 </div>
                 </>
               :
-              <>
-              <Avatar className={clsx( classes.avatar, classes[getCharacterColor(userInfo.firstName.charAt(0))])}>
-                <Typography component="h1" variant="h3" color="inherit">
-                  {userInfo.firstName.charAt(0).toUpperCase()}
+              <div>
+                <Avatar className={clsx( classes.avatar, classes[getCharacterColor(userInfo.firstName.charAt(0))])}>
+                  <Typography component="h1" variant="h3" color="inherit">
+                    {userInfo.firstName.charAt(0).toUpperCase()}
+                  </Typography>
+                </Avatar>
+                <Typography className={classes.text} component="h1" variant="h3" color="inherit">
+                  {`${userInfo.firstName} ${userInfo.lastName}`}
                 </Typography>
-              </Avatar>
-              <Typography className={classes.text} component="h1" variant="h3" color="inherit">
-                {`${userInfo.firstName} ${userInfo.lastName}`}
-              </Typography>
-              <Typography className={classes.text} variant="h5" color="inherit" paragraph>
-                  Description
-              </Typography>
-              </>
+                <Typography className={classes.text} variant="h5" color="inherit" paragraph>
+                    Description
+                </Typography>
+              </div>
             }            
         </div>
     </Paper>
