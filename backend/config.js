0bf050f5ecf0d08c6500048a7a9ba95651ae16c8
AtticusFinchIPH <tranvan.duc.012@gmail.com>
2020-08-24 15:26:53 +0200

    Add PORT to config

--- backend/config.js
+++ backend/config.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export default {
-    PORT: 5000,
+    PORT: process.env.PORT || 5000,
     MONGODB_URL: process.env.MONGODB_URL || 'mongodb://localhost/atticus-social',
     JWT_SECRET: process.env.JWT_SECRET || 'somethingsecret',
     USER_IMAGES_PATH: process.env.USER_IMAGES_PATH || '/../../frontend/public/userImages',
