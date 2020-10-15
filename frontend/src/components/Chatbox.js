76d6d9119996a97cbf983c19a81260eec6466f64
AtticusFinchIPH <tranvan.duc.012@gmail.com>
2020-10-15 17:48:27 +0200

    Decorate Chatbox

--- frontend/src/components/Chatbox.js
+++ frontend/src/components/Chatbox.js
@@ -1,9 +1,15 @@
 import React from "react";
+import clsx from "clsx";
+import { deepOrange, lightBlue, deepPurple } from '@material-ui/core/colors'
 import { withStyles, makeStyles } from "@material-ui/styles";
-import { AppBar, Paper, Toolbar, IconButton, Tooltip } from "@material-ui/core";
+import { AppBar, Paper, Toolbar, IconButton, Tooltip, Avatar, TextField, Divider } from "@material-ui/core";
 import Typography from "@material-ui/core/Typography";
 import MinimizeIcon from '@material-ui/icons/Minimize';
 import CloseIcon from '@material-ui/icons/Close';
+import { useSelector } from "react-redux";
+import { getCharacterColor } from "../util";
+
+const AVATAR_S_DIMENSION = 4;
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -12,7 +18,6 @@ const useStyles = makeStyles((theme) => ({
         bottom: theme.spacing(0),
         right: theme.spacing(10),
         width: theme.spacing(40),
-        height: theme.spacing(45),
     },
     appbar: {
         height: theme.spacing(6),
@@ -23,6 +28,16 @@ const useStyles = makeStyles((theme) => ({
         paddingLeft: theme.spacing(1),
         paddingRight: theme.spacing(1),
     },
+    chatSection: {
+        height: theme.spacing(35),
+        overflowY: "scroll",
+    },
+    chatMaker: {
+        height: theme.spacing(6),
+        display: 'flex',
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+    },
     chatboxName: {
         maxWidth: theme.spacing(30),
         overflow: "hidden",
@@ -30,8 +45,29 @@ const useStyles = makeStyles((theme) => ({
     iconButton: {
         color: "#ffffff"
     },
+    avatarSmall: {
+        width: theme.spacing(AVATAR_S_DIMENSION),
+        height: theme.spacing(AVATAR_S_DIMENSION),
+        margin: theme.spacing(1),
+    },
+    textField: {
+        width: "90%",
+        marginRight: theme.spacing(2),
+    },
     grow:{
       flexGrow: 1,
+    },
+    orange: {
+        color: theme.palette.getContrastText(deepOrange[500]),
+        backgroundColor: deepOrange[500],
+    },
+    blue: {
+      color: theme.palette.getContrastText(lightBlue[500]),
+      backgroundColor: lightBlue[500],
+    },
+    purple: {
+      color: theme.palette.getContrastText(deepPurple[500]),
+      backgroundColor: deepPurple[500],
     },
 }));
 
@@ -46,6 +82,8 @@ const LightTooltip = withStyles((theme) => ({
 
 const Chatbox = () => {
     const classes = useStyles();
+    const userSignin = useSelector(state => state.userSignin);
+    const {userInfo} = userSignin;
     return (
         <>
         <Paper className={classes.root} elevation={3} >
@@ -67,6 +105,26 @@ const Chatbox = () => {
                     </LightTooltip>
                 </Toolbar>
             </AppBar>
+            <div className={classes.chatSection}>
+
+            </div>
+            <Divider width="100%"/>
+            <div className={classes.chatMaker}>
+                <Avatar className={clsx( classes.avatarSmall, classes[getCharacterColor(userInfo.firstName.charAt(0))])}>
+                    <Typography component="h6" variant="h6" color="inherit">H
+                        {/* {userInfo.firstName.charAt(0).toUpperCase()} */}
+                    </Typography>
+                </Avatar>
+                <TextField
+                    size="samll"
+                    // variant="outlined"
+                    // value={curComment}
+                    // onChange={(e) => setCurComment(e.target.value)}
+                    // onKeyDown={submitComment}
+                    className={classes.textField}
+                    margin="normal"
+                />
+            </div>
         </Paper>
         </>
     );
